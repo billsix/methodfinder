feb5477c0dfcf6c6e04a712807d7a5c11f6eaba3
William Emerison Six <billsix@gmail.com>
2019-06-23 20:01:40 -0500

    added in basic support for __special__ methods

--- README.md
+++ README.md
@@ -7,12 +7,29 @@ methodfinder.find tries to find the name.
 
 ```python
 >>> import methodfinder
->>> methodfinder.find(argList=[-1], desiredResult=1)
-('-1', '__abs__')
-('-1', '__bool__')
-('-1', '__neg__')
-('-1', 'bit_length')
-('-1', 'denominator')
->>> methodfinder.find(argList=[" ",["foo", "bar"]], desiredResult="foo bar")
-(' ', 'join', ["['foo', 'bar']"])
+>>> methodfinder.find(objects=[" ",["foo", "bar"]], desiredResult="foo bar")
+" ".join(['foo', 'bar'])
+>>>
+>>> methodfinder.find(objects=[3], desiredResult="3")
+3.__repr__
+str(3)
+>>>
+>>> methodfinder.find(objects=[-1,3], desiredResult=2)
+-1+3
+-1%3
+-1.__radd__(3)
+3+-1
+3.__radd__(-1)
+3.__rmod__(-1)
+>>>
+>>> methodfinder.find(objects=[3,2], desiredResult=1.5)
+3/2
+2.__rtruediv__(3)
+>>>
+>>> methodfinder.find(objects=[-1], desiredResult=1)
+abs(-1)
+bool(-1)
+-(-1)
+-1.bit_length
+-1.denominator
 ```
--- src/methodfinder/methodfinder.py
+++ src/methodfinder/methodfinder.py
@@ -20,21 +20,31 @@
 
 import itertools
 
-def find(argList, desiredResult):
+def _str(obj):
+    """Print, but if it's a string, include quotes
+
+"""
+    if isinstance(obj, str):
+        return '"' + str(obj) + '"'
+    else:
+        return str(obj)
+
+
+def find(objects, desiredResult):
     """Sometimes you know the inputs and outputs for a procedure, but you don't remember the name.
     methodfinder.find tries to find the name.
 
     >>> import methodfinder
-    >>> methodfinder.find(argList=[-1], desiredResult=1)
-    ('-1', '__abs__')
-    ('-1', '__bool__')
-    ('-1', '__neg__')
-    ('-1', 'bit_length')
-    ('-1', 'denominator')
-    >>> methodfinder.find(argList=[" ",["foo", "bar"]], desiredResult="foo bar")
-    (' ', 'join', ["['foo', 'bar']"])
+    >>> methodfinder.find(objects=[-1], desiredResult=1)
+    abs(-1)
+    bool(-1)
+    -(-1)
+    -1.bit_length
+    -1.denominator
+    >>> methodfinder.find(objects=[" ",["foo", "bar"]], desiredResult="foo bar")
+    " ".join(['foo', 'bar'])
 """
-    objectPermutations = itertools.permutations(argList)
+    objectPermutations = itertools.permutations(objects)
     for p in objectPermutations:
         firstArg = p[0]
         rest = p[1:]
@@ -43,13 +53,30 @@ def find(argList, desiredResult):
         for d in dirs:
             attribute = getattr(firstArg, d)
             if attribute == desiredResult:
-                print((str(firstArg), d))
+                print(_str(firstArg)+"."+str(d))
             if callable(attribute):
                 try:
                     if attribute(*rest) == desiredResult:
-                        if len(rest) == 0:
-                            print((str(firstArg), d))
+                        if not rest:
+                            prefixBuiltins = {"__abs__": "abs",
+                                              "__bool__": "bool",
+                                              "__neg__": "-",
+                                              "__str__": "str"}
+                            if d in prefixBuiltins.keys():
+                                print(prefixBuiltins[d]+"("+str(firstArg)+")")
+                            else:
+                                print(_str(firstArg)+"."+str(d))
                         else:
-                            print((str(firstArg), d, list(map(str, rest))))
+                            infixBuiltins = {"__add__": "+",
+                                             "__mod__": "%",
+                                             "__sub__": '-',
+                                             "__mul__": '*',
+                                             "__truediv__": '/'}
+                            argListToPrint = str(list(map(_str, rest)))
+                            if d in infixBuiltins.keys():
+                                print(_str(firstArg) + infixBuiltins[d] + argListToPrint[2:-2])
+                            else:
+                                argListToPrint = str(list(map(_str, rest)))
+                                print(_str(firstArg)+ "." + d + "(" + argListToPrint[2:-2] + ")")
                 except:
                     pass
