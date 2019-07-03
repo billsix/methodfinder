3dcb370eb33ea711192a2486c8639031e4908542
William Emerison Six <billsix@gmail.com>
2019-07-03 07:16:01 -0500

    Added doctest.

--- src/methodfinder/methodfinder.py
+++ src/methodfinder/methodfinder.py
@@ -22,33 +22,35 @@ import itertools
 import copy
 
 
-def testForEquality(o1, o2):
-    try:
-        # take 100 elements from them.  any user of methodfinder
-        # will not be putting in more than 100 elements
-        # if it's not an iterator, an exception will be thrown
-        for e1, e2 in list(itertools.zip_longest(list(itertools.islice(o1, 100)),
-                                                 list(itertools.islice(o2, 100)))):
-            if not testForEquality(e1, e2):
-                return False
-        return True
-    except:
-        return o1 == o2
-
-
 def find(*objects, whichEvaluatesTo):
     """Sometimes you know the inputs and outputs for a procedure, but you don't remember the name.
     methodfinder.find tries to find the name.
 
     >>> import methodfinder
+    >>> import itertools
+    >>> methodfinder.find(" ",["foo", "bar"], whichEvaluatesTo="foo bar")
+    ' '.join(['foo', 'bar'])
+    >>> methodfinder.find(itertools, [1,2], whichEvaluatesTo=[[1,2],[2,1]])
+    <module 'itertools' (built-in)>.permutations([1, 2])
+    >>> methodfinder.find(itertools, [1,2], [3,4], whichEvaluatesTo=[[1,3],[2,4]])
+    <module 'itertools' (built-in)>.zip_longest([1, 2], [3, 4])
+    >>> methodfinder.find([], whichEvaluatesTo=False)
+    len([])
+    >>> methodfinder.find(3, whichEvaluatesTo="3")
+    repr(3)
+    str(3)
+    >>> methodfinder.find(-1,3, whichEvaluatesTo=2)
+    -1+3
+    -1%3
+    3+-1
+    >>> methodfinder.find(3,2, whichEvaluatesTo=1.5)
+    3/2
     >>> methodfinder.find(-1, whichEvaluatesTo=1)
     abs(-1)
     bool(-1)
     -(-1)
-    -1.bit_length
+    -1.bit_length()
     -1.denominator
-    >>> methodfinder.find(" ",["foo", "bar"], whichEvaluatesTo="foo bar")
-    " ".join(['foo', 'bar'])
 """
     # so that all objects which are deep-copiable can be copied
     # while not failing on objects which can't (i.e. modules)
@@ -60,13 +62,34 @@ def find(*objects, whichEvaluatesTo):
                 yield o
 
     for firstObject, *restObjects in deep_copy_all_objects(itertools.permutations(objects)):
-        for d in dir(firstObject):
-            attribute = getattr(firstObject, d)
+        for attributeName, attribute in [(d, getattr(firstObject, d)) for d in dir(firstObject)]:
             if attribute == whichEvaluatesTo:
-                print(repr(firstObject)+"."+str(d))
+                print(repr(firstObject)+"."+str(attributeName))
             elif callable(attribute):
                 try:
-                    if testForEquality(attribute(*restObjects), whichEvaluatesTo):
+                    def testForEqualityNestedly(o1, o2):
+                        try:
+                            # if they have iterators, no exception will be thrown
+
+                            # take 100 elements from them.  any user of methodfinder
+                            # will not be putting in more than 100 elements
+                            # if it's not an iterator, an exception will be thrown
+                            for e1, e2 in itertools.zip_longest(itertools.islice(o1, 100),
+                                                                itertools.islice(o2, 100)):
+                                if not testForEqualityNestedly(e1, e2):
+                                    return False
+                            return True
+                        except:
+                            # since at least one of the objects does not have an iterator,
+                            # just test for equality normally
+                            return o1 == o2
+
+                    # try all of the permutations of methods.  If a method call
+                    # fails, an exception will be thrown and ignored below.
+                    # if the method's result equals the desired result, print
+                    # out the expression
+
+                    if testForEqualityNestedly(attribute(*restObjects), whichEvaluatesTo):
                         if not restObjects:
                             prefixBuiltins = {"__abs__": "abs",
                                               "__bool__": "bool",
@@ -75,29 +98,32 @@ def find(*objects, whichEvaluatesTo):
                                               "__str__": "str",
                                               "__len__": "len"
                                               }
-                            if d in prefixBuiltins.keys():
-                                print(prefixBuiltins[d] +
+                            if attributeName in prefixBuiltins.keys():
+                                print(prefixBuiltins[attributeName] +
                                       "("+str(firstObject)+")")
                             else:
-                                print(repr(firstObject)+"."+str(d)+"()")
+                                print(repr(firstObject)+"." +
+                                      str(attributeName)+"()")
                         else:
                             toSkip = ["__rmod__",
                                       "__radd__",
                                       "__rtruediv__"
                                       ]
-                            if d in toSkip:
+                            if attributeName in toSkip:
                                 continue
                             infixBuiltins = {"__add__": "+",
                                              "__mod__": "%",
                                              "__sub__": '-',
                                              "__mul__": '*',
                                              "__truediv__": '/'}
-                            argListToPrint = repr([list(restObjects)])
-                            if d in infixBuiltins.keys():
+                            argListToPrint = repr([list(restObjects)])[2:-2]
+                            if attributeName in infixBuiltins.keys():
                                 print(repr(firstObject) +
-                                      infixBuiltins[d] + argListToPrint[2:-2])
+                                      infixBuiltins[attributeName] + argListToPrint)
                             else:
-                                print(repr(firstObject) + "." + d +
-                                      "(" + argListToPrint[2:-2] + ")")
+                                print(repr(firstObject) + "." + attributeName +
+                                      "(" + argListToPrint + ")")
                 except:
+                    # the method call failed, so the result is not the desired result,
+                    # so do nothing
                     pass
